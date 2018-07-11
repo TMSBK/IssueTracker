4b53c591e033d0d93bc2200e0bd89da6c272670d
root <you@example.com>
2018-07-11 16:16:54 +0200

    Added some fetching features Added some css

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ hamburger.addEventListener('click', function() {
 		navContainer.setAttribute('style', 'top: 0');
 		navFlag = true;
 	} else {
-		navContainer.setAttribute('style', 'top: -150%');
+		navContainer.setAttribute('style', 'top: -225%');
 		setTimeout(function() {
 			navContainer.classList.remove('transition');
 		}, 400);
@@ -41,6 +41,8 @@ firstNav.addEventListener('click', function() {
 /* ==================== BEJELENTŐ FORM ==================== */
 
 
+/* Ez a kód kezeli az értékelő pontokat */
+
 let radioButtonValue = '';
 
 function ratingTracker(radioButton) {
@@ -58,83 +60,13 @@ function ratingTracker(radioButton) {
 	}
 }
 
-var x, i, j, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
-x = document.getElementsByClassName("custom-select");
-for (i = 0; i < x.length; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  /*for each element, create a new DIV that will act as the selected item:*/
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  /*for each element, create a new DIV that will contain the option list:*/
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < selElmnt.length; j++) {
-    /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /*when an item is clicked, update the original select box,
-        and the selected item:*/
-        var y, i, k, s, h;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < s.length; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            for (k = 0; k < y.length; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            break;
-          }
-        }
-        h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
-      /*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
-  });
-}
-function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
-  var x, y, i, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  for (i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);
+
+
+/* Itt kezeljük a beállítások hamburger menüt */
 
 const containerOn = document.getElementById('selectContainerOn');
 const containerOff = document.getElementById('selectContainerOff');
 const selectContainer = document.getElementById('selectContainer');
-const body = document.getElementsByTagName('body')[0];
 
 containerOn.addEventListener('click', function() {
     selectContainer.classList.add('transition');
@@ -142,8 +74,27 @@ containerOn.addEventListener('click', function() {
 });
 
 containerOff.addEventListener('click', function() {
-      selectContainer.style.top = '-320px';
+    selectContainer.style.top = '-100%';
     setTimeout(function() {
-      selectContainer.classList.remove('transition');
+        selectContainer.classList.remove('transition');
     }, 400);
 });
+
+/* Itt töltjük fel a beállítások hamburger menüt adatokkal */
+
+function fillSelectItemShell(options) {
+  let selectItemShell = document.getElementsByClassName('selectItemShell');
+  for (let i = 0; i < selectItemShell.length; i++) {
+      selectItemShell[i].innerHTML = options.options[i].name;
+  }
+}
+
+fetch('options.json', {
+    })
+        .then(response => response.json())
+        .then(optionsJSON => {
+              let options = optionsJSON; 
+              fillSelectItemShell(options); 
+        })
+
+
