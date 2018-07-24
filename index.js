02c375da633347f08e1043143c46b43a637042d8
root <you@example.com>
2018-07-24 15:59:02 +0200

    Started to create the issues panel

--- index.js
+++ index.js
@@ -16,121 +16,238 @@ hamburger.addEventListener('click', function() {
   	}
 });
 
-// Get the link element that references the templates.html file.
-var templatesImport = document.getElementById('templates');
+function contentLoader(templateName, functionName) {
 
-// Retrieve the loaded templates.
-var templates = templatesImport.import
+    fetch('Templates/templates.html')
+        .then(function(response) {
+            // Lehívom a template-eket és textté konvertálom őket
+            return response.text()
+        })
+        .then(function(html) {
+    
+            let parser = new DOMParser(); // Beállítom a DOM parsert
+            let doc = parser.parseFromString(html, "text/html"); // Stringből HTML-lé alakítom a template-eket
+            let usedTemplate = doc.getElementById(templateName); // Itt választom ki a template nevét
+            document.getElementById('main').innerHTML = ''; // Itt kiürítem a felületet
+            document.getElementById('main').appendChild(usedTemplate.content); // Itt betöltöm a felületet
+            functionName(); // Meghívom a template-hez tartozó scriptet
+        })
+}
+
+document.getElementById('1').addEventListener('click', function(){
+    contentLoader('form', formHelper);
+});
+
+document.getElementById('2').addEventListener('click', function(){
+    contentLoader('issues', issueHelper);
+});
+
 
 /* =============================================== BEJELENTŐ FORM ================================================ */
-
-// Get the template.
-var formTemplate = templates.getElementById('form');
-
-// Clone the template content.
-var formTemplateClone = document.importNode(formTemplate.content, true);
-
-// Add the blog post to the page.
-document.getElementById('main').appendChild(formTemplateClone);
-
-
+ 
 /* Ez a kód kezeli az értékelő pontokat */
 
 let radioButtonValue = '';
 
 function ratingTracker(radioButton) {
-  	radioButtonValue = radioButton.value;
-  	const ratingSign = document.getElementById('ratingSign');
-  	ratingSign.textContent = radioButtonValue + ' pont';
-  	for (let k=1; k <= 5; k++) {
-    	let radioElement = document.getElementById(k + 'p');
-    	if (k<=radioButtonValue) {
-    		radioElement.classList.add('radioInputSpanBackground');
-    	} else {
-    		radioElement.classList.remove('radioInputSpanBackground');
-    	}
-  	}
-}
-
-
-
-/* Itt kezeljük a beállítások hamburger menüt */
-
-const containerOn = document.getElementById('selectContainerOn');
-const containerOff = document.getElementById('selectContainerOff');
-const selectContainer = document.getElementById('selectContainer');
-let selectShell = document.getElementsByClassName('selectShell');
-
-containerOn.addEventListener('click', function() {
-    selectContainer.classList.add('transition');
-    selectContainer.style.top = '0';
-});
-
-containerOff.addEventListener('click', function() {
-    selectContainer.style.top = '-100%';
-    setTimeout(function() {
-        selectContainer.classList.remove('transition');
-    }, 400);
-});
-
-/* Itt szedjük le az adatokat a JSON-ból */
-
-fetch('options.json', {
-    })
-        .then(response => response.json())
-        .then(optionsJSON => {
-              let options = optionsJSON; 
-              fillSelectShell(options); 
-        })
-
-/* Itt töltjük fel a beállítások hamburger menüt adatokkal */
-
-function fillSelectShell(options) {
-    for (let i = 0; i < selectShell.length; i++) {
-        selectShell[i].getElementsByTagName('p')[0].innerHTML = options.options[i].name;
-        let value = Object.values(options.options[i].values);
-        let back = document.createElement('div');
-        back.innerHTML = options.options[i].name;
-        back.classList.add('optionShell');
-        back.style.display = 'none';
-        selectShell[i].append(back);
-        back.addEventListener('click', function() {
-            selectShell[i].getElementsByTagName('p')[0].innerHTML = options.options[i].name;
-            selectShell[i].getElementsByTagName('option')[0].value = options.options[i].name;
-        });
-        for(let j = 0; j < value.length; j++) {
-            let option = document.createElement('div');
-            option.innerHTML = value[j];
-            option.classList.add('optionShell');
-            option.style.display = 'none';
-            option.addEventListener('click', function() {
-                selectShell[i].getElementsByTagName('p')[0].innerHTML = this.innerHTML;
-                selectShell[i].getElementsByTagName('option')[0].value = this.innerHTML;
-            });
-            selectShell[i].append(option);
-        }    
+    radioButtonValue = radioButton.value;
+    const ratingSign = document.getElementById('ratingSign');
+    ratingSign.textContent = radioButtonValue + ' pont';
+    for (let k=1; k <= 5; k++) {
+        let radioElement = document.getElementById(k + 'p');
+        if (k<=radioButtonValue) {
+            radioElement.classList.add('radioInputSpanBackground');
+        } else {
+            radioElement.classList.remove('radioInputSpanBackground');
+        }
     }
 }
 
-/* Itt kezeljük a legördülőket */
+function formHelper() {
 
-let optionShell = document.getElementsByClassName('optionShell');
+    /* Itt kezeljük a beállítások hamburger menüt */
 
-for(let i = 0; i < selectShell.length; i++) {
-    let optionShellArray = selectShell[i].getElementsByClassName('optionShell');
-    selectShell[i].addEventListener('click', function() {
-        if(this.getElementsByClassName('optionShell')[0].style.display === 'none') {
-            for(let j = 0; j < optionShell.length; j++) {
-                optionShell[j].style.display = 'none';
-            }
-            for(let j = 0; j < optionShellArray.length; j++) {
-                optionShellArray[j].style.display = 'block';
-            }
-        } else {
-            for(let j = 0; j < optionShellArray.length; j++) {
-                optionShellArray[j].style.display = 'none';
-            }
+    const containerOn = document.getElementById('selectContainerOn');
+    const selectContainer = document.getElementById('selectContainer');
+
+    containerOn.addEventListener('click', function() {
+        selectContainer.classList.add('transition');
+        selectContainer.style.top = '0';
+    });
+
+    /* Itt szedjük le az adatokat a JSON-ból */
+
+    fetch('options.json', {
+        })
+            .then(response => response.json())
+            .then(optionsJSON => {
+                  let options = optionsJSON; 
+                  fillSelectShell(options); 
+            })
+
+    /* Itt töltjük fel a beállítások hamburger menüt adatokkal */
+
+    let html = '<p></p><select><option></option></select>' // Ezzel töltjük fel a selectShell-eket
+    let optionShells = document.getElementsByClassName('optionShell'); // Ezek az opció maszkok
+
+    function fillSelectShell(options) {
+
+        for (let i = 0; i < options.options.length; i++) { // Végig iterálunk a JSON-on
+
+            let createdSelectShell = document.createElement('div'); // Itt generálunk selectShell-eket a JSON alapján
+            createdSelectShell.innerHTML = html; // Itt töltjük fel őket
+            createdSelectShell.classList.add('selectShell'); // Itt adunk class-t hozzá
+            selectContainer.append(createdSelectShell); // Itt adjuk hozzá a konténerhez
+            let usedOptionShells = createdSelectShell.getElementsByClassName('optionShell');
+
+            createdSelectShell.addEventListener('click', function() {
+                if(this.getElementsByClassName('optionShell')[0].style.display === 'none') {
+                    for(let i = 0; i < optionShells.length; i++) {
+                        optionShells[i].style.display = 'none';
+                    }
+                    for(let i = 0; i < usedOptionShells.length; i++) {
+                        usedOptionShells[i].style.display = 'block';
+                    }
+                } else {
+                    for(let i = 0; i < usedOptionShells.length; i++) {
+                        usedOptionShells[i].style.display = 'none';
+                    }
+                }
+            });
+
+            // Itt adjuk meg az elrejtett option/select elemek tulajdonságait
+
+            document.getElementsByTagName('select')[i].name = options.options[i].name;
+            document.getElementsByTagName('option')[i].value = options.options[i].name;
+            document.getElementsByTagName('select')[i].classList.add('selectItem');
         }
-    })
+
+        let selectShells = document.getElementsByClassName('selectShell');
+
+        for (let i = 0; i < selectShells.length; i++) {
+            selectShells[i].getElementsByTagName('p')[0].innerHTML = options.options[i].name;
+            let value = Object.values(options.options[i].values);
+            let back = document.createElement('div');
+            back.innerHTML = options.options[i].name;
+            back.classList.add('optionShell');
+            back.style.display = 'none';
+            selectShells[i].append(back);
+            back.addEventListener('click', function() {
+                selectShells[i].getElementsByTagName('p')[0].innerHTML = options.options[i].name;
+                selectShells[i].getElementsByTagName('option')[0].value = options.options[i].name;
+            });
+
+            for(let j = 0; j < value.length; j++) {
+                let option = document.createElement('div');
+                option.innerHTML = value[j];
+                option.classList.add('optionShell');
+                option.style.display = 'none';
+                option.addEventListener('click', function() {
+                    selectShells[i].getElementsByTagName('p')[0].innerHTML = this.innerHTML;
+                    selectShells[i].getElementsByTagName('option')[0].value = this.innerHTML;
+                });
+
+                selectShells[i].append(option);
+            }    
+        }
+
+        let close = document.createElement('div');
+        close.innerHTML = 'VISSZA';
+        close.setAttribute('id', 'selectContainerOff');
+        close.addEventListener('click', function() {
+            selectContainer.style.top = '-100%';
+            setTimeout(function() {
+                selectContainer.classList.remove('transition');
+            }, 400);
+        });
+
+        selectContainer.append(close);
+    }
+
 }
 
+/* =============================================== BEJELENTÉSEK ================================================ */
+
+function issueHelper() {
+
+    /* Itt szedjük le az adatokat a JSON-ból */
+
+    fetch('issues.json', {
+        })
+            .then(response => response.json())
+            .then(issuesJSON => {
+                  let datas = issuesJSON; 
+                  fillIssue(datas);
+            })    
+
+    let html = '<h3></h3>';
+    let issueContainer = document.getElementById('issueContainer');
+    const issuePanel = document.getElementById('issuePanel'); 
+
+    function fillIssue(datas) {
+        for(let i = 0; i < datas.issues.length; i++) {
+            let createdIssue = document.createElement('div'); // Itt generálunk selectShell-eket a JSON alapján
+            createdIssue.innerHTML = html; // Itt töltjük fel őket
+            createdIssue.classList.add('issue'); // Itt adunk class-t hozzá
+            let issues = document.getElementsByClassName('issue'); 
+            issueContainer.append(createdIssue); // Itt adjuk hozzá a konténerhez
+            issues[i].getElementsByTagName('h3')[0].innerHTML = datas.issues[i].issue;
+
+            if (datas.issues[i].Státusz === 'Bejelentve') {
+                createdIssue.style.backgroundColor = 'brown';
+                createdIssue.classList.add('active');
+            } 
+            else if (datas.issues[i].Státusz === 'Munkafelvétel') {
+                createdIssue.style.backgroundColor = 'purple';
+                createdIssue.classList.add('active');
+            } 
+            else if (datas.issues[i].Státusz === 'Jóváhagyásra') {
+                createdIssue.style.backgroundColor = 'coral';
+                createdIssue.classList.add('active');
+            } 
+            else if (datas.issues[i].Státusz === 'Végrehajtásra') {
+                createdIssue.style.backgroundColor = 'DeepSkyBlue';
+                createdIssue.classList.add('active');
+            } 
+            else if (datas.issues[i].Státusz === 'TeljesítésIgazolásra') {
+                createdIssue.style.backgroundColor = 'orange';
+                createdIssue.classList.add('finished');
+            } 
+            else if (datas.issues[i].Státusz === 'Ellenőrzésre') {
+                createdIssue.style.backgroundColor = 'yellow';
+                createdIssue.classList.add('finished');
+            } 
+            else if (datas.issues[i].Státusz === 'Lezárva') {
+                createdIssue.style.backgroundColor = 'green';
+                createdIssue.classList.add('finished');
+            } 
+
+            issuePanel.innerHTML = '<h3></h3><h3></h3><h3></h3><h3></h3><h3></h3><h3></h3><h3></h3><h3></h3><h3></h3><h3></h3><h3></h3>';
+            
+            issues[i].addEventListener('click', function() {
+                issuePanel.getElementsByTagName('h3')[0].innerHTML = 'Bejelentés száma: ' + datas.issues[i].id;
+                issuePanel.getElementsByTagName('h3')[1].innerHTML = 'Bejelentés státusza: ' + datas.issues[i].Státusz;
+                issuePanel.getElementsByTagName('h3')[2].innerHTML = 'Bejelentés időpontja: ' + datas.issues[i].Bejelentve;
+                issuePanel.getElementsByTagName('h3')[3].innerHTML = 'Kiérkezés várható időpontja: ' + datas.issues[i].Várható_Kezdés;
+                issuePanel.getElementsByTagName('h3')[4].innerHTML = 'Objektum megnevezése: ' + datas.issues[i].Objektum;
+                issuePanel.getElementsByTagName('h3')[5].innerHTML = 'Ingatlan megnevezése: ' + datas.issues[i].Ingatlan;
+                issuePanel.getElementsByTagName('h3')[6].innerHTML = 'Szakterület: ' + datas.issues[i].Szakterület;
+                issuePanel.getElementsByTagName('h3')[7].innerHTML = 'Típus: ' + datas.issues[i].Típus;
+                issuePanel.getElementsByTagName('h3')[8].innerHTML = 'Besorolás: ' + datas.issues[i].Besorolás;
+                issuePanel.getElementsByTagName('h3')[9].innerHTML = 'Szolgáltatási szint: ' + datas.issues[i].Sz_szint;
+                issuePanel.getElementsByTagName('h3')[10].innerHTML = 'Bejelentés tartalma: ' + datas.issues[i].issue;
+                issuePanel.style.display = 'block';
+            });
+        }
+    }
+}
+
+let createdIssues = document.getElementsByClassName('finished');
+
+function filter() {
+    for(let i = 0; i < createdIssues.length; i++) {
+        if(createdIssues[i].classList.contains('finished')) {
+            createdIssues[i].style.display = 'none';
+        }
+    }
+}
